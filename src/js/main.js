2d6855dd5c795ff3cf87cce2444a33907c0e652c
haripallikere <haripallikere93@gmail.com>
2018-10-28 18:55:08 +0530

    Fiz Error with Main.js and main.css

--- src/js/main.js
+++ src/js/main.js
@@ -44,12 +44,12 @@ const displayFavourite = (array, parent) => {
     parent.innerHTML += 
     `
     <div class="favoruite-restrurantCard">
-      <h1 class="fav-head" Name - <span>${item.restaurant.name}</span></h1>
-      <a class="link" href="${item.restaurant.url}"> website </a>
-      <h2 class="fav-head2" Address <span>${item.restaurant.location.city}</span></h2>
-      <h2 class="fav-vote" Votes <span>${item.restaurant.user_rating.aggregate_rating}</span></h2>
-      <button class="delete-favourite"  data-id=${i}>Delete Favourite<g/button>
-    </div>
+    <h1 class="fav-head" Name - <span>${item.restaurant.name}</span></h1>
+    <a class="link" href="${item.restaurant.url}"> website </a>
+    <h2 class="fav-head2" Address <span>${item.restaurant.location.city}</span></h2>
+    <h2 class="fav-vote" Votes <span>${item.restaurant.user_rating.aggregate_rating}</span></h2>
+    <button class="delete-favourite"  data-id=${i}>Delete Favourite<g/button>
+  </div>
     `
   }).join('');
 }
