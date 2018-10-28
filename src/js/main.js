b06692457cd80da33636c87f4e5bfcdabde55877
praveen-me <praveenkrsaini0@gmail.com>
2018-10-28 17:06:21 +0530

    Add Favoruites Section

--- src/js/main.js
+++ src/js/main.js
@@ -12,14 +12,31 @@ const display = (array, parent) => {
   array.length === 0 ? "" : array.map((item,i) => {
     parent.innerHTML += 
     `
-    <div class="restrurantCard" id=${i}>
+    <div class="restrurantCard">
       <h1>Name - <span>${item.restaurant.name}</span></h1>
       <a>Url <span>${item.restaurant.url}</span></a>
       <h2>Address <span>${item.restaurant.location.address}</span></h2>
       <h2>Votes <span>${item.restaurant.user_rating.aggregate_rating}</span></h2>
       <h2>Rating <span>${item.restaurant.user_rating.votes}</span></h2>
       <h2>Average Cost of Two <span>${item.restaurant.average_cost_for_two}</span></h2>
-      <button class="add-favourite">Add Favourite<g/button>
+      <button class="add-favourite"  data-id=${i}>Add Favourite<g/button>
+    </div>
+    `
+  }).join('');
+}
+
+const displayFavourite = (array, parent) => {
+  parent.innerHTML = '';
+
+  array.length === 0 ? "" : array.map((item,i) => {
+    parent.innerHTML += 
+    `
+    <div class="favoruite-restrurantCard">
+      <h1>Name - <span>${item.restaurant.name}</span></h1>
+      <a>Url <span>${item.restaurant.url}</span></a>
+      <h2>Address <span>${item.restaurant.location.city}</span></h2>
+      <h2>Votes <span>${item.restaurant.user_rating.aggregate_rating}</span></h2>
+      <button class="delete-favourite"  data-id=${i}>Delete Favourite<g/button>
     </div>
     `
   }).join('');
@@ -38,7 +55,7 @@ const searchRestrurants = e => {
 	}
   }).then(res => {console.log(res.status);return res.json()}).then(data => {
     restrurants = data.restaurants;
-    console.log(restrurants);
+    console.log(restrurants[0]);
     display(restrurants, list);
   })
 
@@ -48,11 +65,21 @@ const searchRestrurants = e => {
 const addToFavourite = e => {
   e.preventDefault();
   if(e.target.classList.contains('add-favourite')) {
-    const itemIndex = e.target.parentElement.id;
+    const itemIndex = e.target.dataset.id;
     favourites.push(restrurants[itemIndex]);
-    display(favourites, favoriteList);
+    displayFavourite(favourites, favoriteList);
   }
 }
 
+const deleteFavourite = e => {
+  e.preventDefault();
+  if(e.target.classList.contains('delete-favourite')) {
+    const itemIndex = e.target.dataset.id;
+    favourites.splice(itemIndex, 1);
+    displayFavourite(favourites, favoriteList);
+  }
+}
+ 
 searchForm.addEventListener("submit", searchRestrurants);
 list.addEventListener('click', addToFavourite);
+favoriteList.addEventListener('click', deleteFavourite);
