680140a5d98aef2fddb114446695f2b45c0f5198
praveen-me <praveenkrsaini0@gmail.com>
2018-10-28 15:34:28 +0530

    Add Restrurants Searching Functionality, Add display template also

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,54 @@
 const searchBtn = document.getElementById('search-btn');
+const searchForm = document.getElementById('search-form');
+const list = document.querySelector('.list');
 
-const searchRestrurants = e => {
-  const searchValue = document.getElementById(".search-value");
+let restrurants = [];
 
-  const url = `https://developers.zomato.com/api/v2.1/search?q=${searchValue.value}`;
 
-  console.log(url);
+const display = (array, parent) => {
+  parent.innerHTML = '';
+
+  restrurantCardStr = `
+    
+  `;
+
+  array.map(item => {
+    return parent.innerHTML += restrurantCardStr;
+  }).join('');
+
+  array.length === 0 ? "" : array.map(item => {
+    parent.innerHTML += 
+    `
+    <div class="restrurantCard">
+      <h1>Name - <span>${item.restaurant.name}</span></h1>
+      <a>Url <span>${item.restaurant.url}</span></a>
+      <h2>Address <span>${item.restaurant.location.address}</span></h2>
+      <h2>Votes <span>${item.restaurant.user_rating.aggregate_rating}</span></h2>
+      <h2>Rating <span>${item.restaurant.user_rating.votes}</span></h2>
+      <h2>Average Cost of Two <span>${item.restaurant.average_cost_for_two}</span></h2>
+    </div>
+    `
+  }).join('');
 }
 
-searchBtn.addEventListener("click", searchRestrurants);
+const searchRestrurants = e => {
+  e.preventDefault();
+  const searchValue = document.getElementById("search-value");
+
+  const url = `https://developers.zomato.com/api/v2.1/search?q=${searchValue.value.toLowerCase()}&count=15`;
+
+  fetch(url, {
+	method : 'GET',
+	headers : {
+		"user-key" : "af90611232a48e9a8cc0279f40717419"
+	}
+  }).then(res => {console.log(res.status);return res.json()}).then(data => {
+    restrurants = data.restaurants;
+    console.log(restrurants);
+    display(restrurants, list);
+  })
+
+  console.log('form submitted')
+}
+
+searchForm.addEventListener("submit", searchRestrurants);
